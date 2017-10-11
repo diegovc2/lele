37bb5c061bde5436ad231b3957f951969759571f
diegovc2 <diegoveloso34@hotmail.com>
2017-10-11 17:40:46 -0300

    donde tiene que estar

--- formularioREAL/subidas.sql
+++ formularioREAL/subidas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-10-2017 a las 22:36:01
+-- Tiempo de generación: 11-10-2017 a las 22:40:19
 -- Versión del servidor: 10.1.26-MariaDB
 -- Versión de PHP: 7.1.8
 
